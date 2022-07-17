7f907cff0fbca625f0a9c2e8382dd8a8ee5ebd75
zaninifrancesco <zaninifrancesco03@gmail.com>
2022-07-17 21:41:09 +0200

    prova fix

--- lib/BottomNavRoutes/todo_example.dart
+++ lib/BottomNavRoutes/todo_example.dart
@@ -1,19 +1,15 @@
-import 'dart:html';
-
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'counter_example.dart';
 
-class ToDoList extends StatefulWidget{
+class ToDoList extends StatefulWidget {
   @override
   State<ToDoList> createState() => _ToDoList();
-  }
+}
 
-class _ToDoList extends State<ToDoList>{
-
-
+class _ToDoList extends State<ToDoList> {
   final sampleList = [];
 
   int _counter = 0;
@@ -53,50 +49,58 @@ class _ToDoList extends State<ToDoList>{
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
-          FloatingActionButton(onPressed: resetList, child: Icon(Icons.restart_alt_sharp),),
+          FloatingActionButton(
+            onPressed: resetList,
+            child: Icon(Icons.restart_alt_sharp),
+          ),
           SizedBox(
             width: 15,
           ),
-          FloatingActionButton(onPressed: addSmthToList, child: Icon(Icons.add_box),),
-
+          FloatingActionButton(
+            onPressed: addSmthToList,
+            child: Icon(Icons.add_box),
+          ),
         ],
       ),
-      body:
-      ListView.builder(
-    itemCount: sampleList.length,
-    itemBuilder: (context, index) {
-      return Padding(padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
-      child: Column(
-      children: [
-        ListTile(
-      title: Text(
-      sampleList[index],
-        style: GoogleFonts.poppins(fontWeight: FontWeight.w500),
-      ),
-        leading: Icon(Icons.account_circle),
-        subtitle: Text("Impegno N.$index"),
-        dense: false,
-        onTap: (() {}),
-        tileColor: Colors.deepPurple[50],
-        trailing: Wrap(
-          children: <Widget>[
-            IconButton(onPressed:  (() => removeItem(index)), icon: Icon(Icons.crop_square_rounded),),
-          ],
-        ),
-        hoverColor: Colors.red,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(7))),
-        ),
-        // Padding(
-        //   padding: const EdgeInsets.symmetric(horizontal: 10),
-        //   child: Divider(
-        //     thickness: 2,
-        //   ),
-        // )
-      ],
-      ),
-      );
-    }),
+      body: ListView.builder(
+          itemCount: sampleList.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
+              child: Column(
+                children: [
+                  ListTile(
+                    title: Text(
+                      sampleList[index],
+                      style: GoogleFonts.poppins(fontWeight: FontWeight.w500),
+                    ),
+                    leading: Icon(Icons.account_circle),
+                    subtitle: Text("Impegno N.$index"),
+                    dense: false,
+                    onTap: (() {}),
+                    tileColor: Colors.deepPurple[50],
+                    trailing: Wrap(
+                      children: <Widget>[
+                        IconButton(
+                          onPressed: (() => removeItem(index)),
+                          icon: Icon(Icons.crop_square_rounded),
+                        ),
+                      ],
+                    ),
+                    hoverColor: Colors.red,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(7))),
+                  ),
+                  // Padding(
+                  //   padding: const EdgeInsets.symmetric(horizontal: 10),
+                  //   child: Divider(
+                  //     thickness: 2,
+                  //   ),
+                  // )
+                ],
+              ),
+            );
+          }),
     );
   }
 }
