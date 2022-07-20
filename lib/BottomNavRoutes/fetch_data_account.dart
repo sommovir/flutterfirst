6735a86ff9e12c72dbb10bc848e9bbf6672567c4
Luca Coraci <luca.coraci@gmail.com>
2022-07-20 17:43:55 +0200

    removed dart:ffi

--- lib/BottomNavRoutes/fetch_data_account.dart
+++ lib/BottomNavRoutes/fetch_data_account.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
