a8229c0fb88caeb3e7a65480029236ec71cd577c
Luca Coraci <luca.coraci@gmail.com>
2022-07-20 19:39:46 +0200

    text with event

--- lib/BottomNavRoutes/todo_widget.dart
+++ lib/BottomNavRoutes/todo_widget.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+Map<String, bool> squareMap = {};
+
 class TodoWidget extends StatefulWidget {
   const TodoWidget({Key? key}) : super(key: key);
 
@@ -10,7 +12,28 @@ class TodoWidget extends StatefulWidget {
   State<TodoWidget> createState() => TodoWidgetState();
 }
 
+int counter = 0;
+
 class TodoWidgetState extends State<TodoWidget> {
+  @override
+  void initState() {
+    counter = 0;
+    squareMap = {
+      "Row 1 Left": false,
+      "Row 1 Right": false,
+      "Row 2 Left": false,
+      "Row 2 Right": false
+    };
+    super.initState();
+  }
+
+  void update(String value) {
+    setState(() {
+      squareMap[value] = true;
+      counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -21,9 +44,17 @@ class TodoWidgetState extends State<TodoWidget> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.center,
-            children: const [
-              Text("Row 1 Left"),
-              Text("Row 1 Right"),
+            children: [
+              Container(
+                color: squareMap["Row 1 Left"] ?? false
+                    ? Colors.blue
+                    : Colors.greenAccent,
+                child: GestureDetector(
+                  onTap: () => update("Row 1 Left"),
+                  child: Text("Text 1 Left: $counter"),
+                ),
+              ),
+              const Text("Row 1 Right"),
             ],
           ),
           Row(
